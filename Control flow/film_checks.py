15e68aac9e9804fc04f19e66714dd78ab2eeb63a
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-04-11 16:41:28 +0100

    added film rating project

--- Control flow/film_checks.py
+++ Control flow/film_checks.py
@@ -1,21 +1,21 @@
 # Possible film ratings are "universal", "pg", "12", "12a", "15", "18"
-film_rating = "12a"
+film_rating = (input("Please submit your film rating : "))
 
 # Use and if statement to check for "universal" rating
-# If Statment goes here
-print("all age groups can watch this film")
-# check if film rating is "pg"
-# Else if statment goes here
-print("General viewing, but some scenes may be unsuitable for young children.")
-# check if film rating is "12" or "12a"
-# Else if statement goes here
+if film_rating == "universal":
+    print("All age groups can watch this film")
 
-print("Films classified 12A and video works classified 12 contain material that is not generally suitable for children aged under 12. No one younger than 12 may see a 12A film in a cinema unless accompanied by an adult.")
+# check if film rating is "pg"
+elif film_rating == "pg":
+    print("General viewing but some scenes might be unsuitable for young children")
+# check if film rating is "12" or "12a"
+elif film_rating == "12" or film_rating == "12a":
+    print("Contains material that is not generally suitable for children aged under 12.")
 # check if film rating is "15"
-# ELSE IF STATEMENT GOES HERE
-print ("No one younger than 15 may see a 15 film in a cinema.")
+elif film_rating == "15":
+    print("No one younger than 15 may see a 15 film in a cinema.")
 # check if film rating is "18"
-# ELSE IF STATEMENT GOES HERE
-print ("No one younger than 18 may see an 18 film in a cinema.")
-# ELSE STATEMENT GOES HERE
-print("This is not a correct rating, please use universal, pg, 12, 12a, 15, 18")
+elif film_rating == "18":
+    print ("No one younger than 18 may see an 18 film in a cinema.")
+else:
+    print("This is not a correct rating, please use universal, pg, 12, 12a, 15, 18")
