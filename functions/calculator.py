42ea8d606fb732a34ba909a4f3d7424f9906a056
joeodx <jojoodonovan@hotmail.com>
2024-04-12 16:29:41 +0100

    added calulator

---
+++ functions/calculator.py
@@ -0,0 +1,6 @@
+# Now, create a calculator script (create calculator.py in your functions folder) that uses functions
+# (function for +, function for -, function for *, function for /) to perform an operation on two ints.
+
+def add(a + b):
+
+
