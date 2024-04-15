9f771da6406b2cfd251f832bd996019f789868e2
joeodx <jojoodonovan@hotmail.com>
2024-04-15 10:57:40 +0100

    added

---
+++ scripting/math_operators.py
@@ -0,0 +1,15 @@
+# Define the add operator
+def add(a, b):
+    return a + b
+
+# Define the minus operator
+def add(c, d):
+    return c - d
+
+# Define the multiply operator
+def add(e, f):
+    return e * f
+
+# Define the divide oeprator
+def add(g, h):
+    return g / h
