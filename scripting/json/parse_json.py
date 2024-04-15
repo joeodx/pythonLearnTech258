fb47289629778c3a17658eaa86479364913c4e19
joeodx <jojoodonovan@hotmail.com>
2024-04-15 15:20:29 +0100

    addedf more info to json file

--- scripting/json/parse_json.py
+++ scripting/json/parse_json.py
@@ -1,9 +1,17 @@
 import json
+#
+# # Converting json to python
+#
+# with open("new_json_file.json") as jsonfile:
+#     car = json.load(jsonfile)
+#     print(type(car))
+#     print(car["name"])
+#     print(car["engine"])
 
-# Converting json to python
 
-with open("new_json_file.json") as jsonfile:
-    car = json.load(jsonfile)
-    print(type(car))
-    print(car["name"])
-    print(car["engine"])
+path_to_json = "example.json"
+json = json.load(open(path_to_json))
+value = json["name"]
+
+print(value)
+
