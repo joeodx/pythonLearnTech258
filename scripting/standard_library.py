1923f17c4f4360133c673b83c144f9d43e131ba8
joeodx <jojoodonovan@hotmail.com>
2024-04-15 11:42:11 +0100

    added

---
+++ scripting/standard_library.py
@@ -0,0 +1,48 @@
+# Python standard library
+# The standard library consists of many modules and packages that are very useful
+# and were thus added to python by default
+
+
+import random
+import math
+import os
+import sys
+import datetime
+
+# import sys
+#
+# # random demo
+# # print(random.random())
+# # print(random.randrange(1,11)) # random number between these two numbers 1-10
+#
+# # math demo
+#
+# # num_float = 23.66
+# # print(math.ceil(num_float)) # round to the closest number up
+# # print(math.floor(num_float)) # round to the closest number down
+# # print(math.pi) # give you pi to 10 decimal places
+# # print(f"remainder from 1/5: {math.remainder(1, 5)}") # gives you the remainder
+#
+#
+# # os demo
+#
+# # returning current working directory
+# working_dir = os.getcwd()
+# print(f"current Working directory is: {working_dir}")
+#
+# # get user
+# username = os.environ.get("USERNAME") or os.environ.get("USER")
+# print(f"Username is: {username}")
+#
+# # cpu cores
+# cpu_cores = os.cpu_count()
+# print(f"Amount of CPUs: {cpu_cores} ")
+#
+# # Make a directory
+# os.mkdir("test_dir")
+
+# # sys demo
+# print(f"Current path: {sys.path}")
+# print(sys.version)
+
+# datetime demo
