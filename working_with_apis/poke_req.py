d29c2a7a7e932395b5f7d4ce15d81e5a529f6886
joeodx <jojoodonovan@hotmail.com>
2024-04-16 16:40:12 +0100

    added while loop

--- working_with_apis/poke_req.py
+++ working_with_apis/poke_req.py
@@ -2,32 +2,18 @@ import requests
 import json
 import random
 
+
 def pokemon_generator():
-    id = random.randint(1,101)
-    return id
+    return random.randint(1, 101)
 
-player_one = input("Enter pokemon ID (number 1-100)")
-player_two = input("Enter pokemon ID (number 1-100)")
 
-# poke_req = requests.get("https://pokeapi.co/api/v2/pokemon/" + str(pokemon_generator()))
-# print(poke_req.json())
-poke_req = requests.get("https://pokeapi.co/api/v2/pokemon/" + str(player_one))
-
-poke_req2 = requests.get("https://pokeapi.co/api/v2/pokemon/" + str(player_two))
-
-# poke_req2 = requests.get("https://pokeapi.co/api/v2/pokemon/" + str(pokemon_generator()))
-# print(poke_req2.json())
-
-poke_data = json.loads(poke_req.text)
-poke_data2 = json.loads(poke_req2.text)
-# print(type(poke_data))
-
-# user_id = input("Enter pokemon ID (number 1-100)")
-
-# two random computer inputs
-print(f"Player one : {poke_data['name']}")
-print(f"Player two : {poke_data2['name']}")
-
+def fetch_pokemon_data(pokemon_id):
+    poke_req = requests.get("https://pokeapi.co/api/v2/pokemon/" + str(pokemon_id))
+    if poke_req.status_code == 200:
+        return json.loads(poke_req.text)
+    else:
+        print("Error fetching Pokémon data. Please try again.")
+        return None
 
 
 def pokemon_compare(player_one, player_two):
@@ -36,9 +22,28 @@ def pokemon_compare(player_one, player_two):
     if player_one["height"] > player_two["height"]:
         return "Player one has won!"
     elif player_one["height"] == player_two["height"]:
-        return "Its a draw!"
+        return "It's a draw!"
     else:
         return "Player two has won!"
 
-print(pokemon_compare(poke_data, poke_data2))
+
+while True:
+    player_one_id = input("Player One, enter Pokémon ID (number 1-100): ")
+    player_two_id = input("Player Two, enter Pokémon ID (number 1-100): ")
+    print("Whoever is the tallest wins!")
+
+    poke_data = fetch_pokemon_data(player_one_id)
+    poke_data2 = fetch_pokemon_data(player_two_id)
+
+    if poke_data and poke_data2:
+        print(f"Player one: {poke_data['name']} (ノಠ益ಠ)ノ")
+        print(f"Player two: {poke_data2['name']} (╯°□°）")
+
+        print(pokemon_compare(poke_data, poke_data2))
+
+    play_again_input = input("Do you want to play again? (yes/no): ").lower()
+    if play_again_input != "yes":
+        break
+
+
 
